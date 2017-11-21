cb425d1b247044e2c26eeed8c0960536beb3a8d3
vishnuvardhan <vishnuvardhanbalasubramanian@gmail.com>
2017-11-21 18:58:01 +0530

    Angular Project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyB__8_sWHf5U1F7BxaTfY9Uvbxn0jeQ-Eo",
+    authDomain: "ecart-a28f7.firebaseapp.com",
+    databaseURL: "https://ecart-a28f7.firebaseio.com",
+    projectId: "ecart-a28f7",
+    storageBucket: "ecart-a28f7.appspot.com",
+    messagingSenderId: "1031035413427"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyB__8_sWHf5U1F7BxaTfY9Uvbxn0jeQ-Eo",
+    authDomain: "ecart-a28f7.firebaseapp.com",
+    databaseURL: "https://ecart-a28f7.firebaseio.com",
+    projectId: "ecart-a28f7",
+    storageBucket: "ecart-a28f7.appspot.com",
+    messagingSenderId: "1031035413427"
+  }
 };
